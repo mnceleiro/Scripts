c8c1a13d3ecbbf4034d983e741c6228128b80e4c
Marcos Núñez Celeiro <Markiusarria@gmail.com>
2023-07-05 11:09:11 +0200

    Actualizado script abalar para ser mais verboso

--- Linux/Abalar/configuracion.sh
+++ Linux/Abalar/configuracion.sh
@@ -8,10 +8,14 @@
 # TODO: preguntar o nome de usuario
 
 # Instalacion de SeaMonkey (navegador web sinxelo)
-wget https://archive.mozilla.org/pub/seamonkey/releases/2.53.16/linux-i686/es-ES/seamonkey-2.53.16.es-ES.linux-i686.tar.bz2
-tar -jxvf seamonkey-2.53.16.es-ES.linux-i686.tar.bz2 -C /opt/
+echo "1. Descargando o navegador web Seamonkey..."
+wget https://archive.mozilla.org/pub/seamonkey/releases/2.53.16/linux-i686/es-ES/seamonkey-2.53.16.es-ES.linux-i686.tar.bz2 > /dev/null
+
+echo "2. Copiando Seamonkey en /opt..."
+tar -jxvf seamonkey-2.53.16.es-ES.linux-i686.tar.bz2 -C /opt/ > /dev/null
 rm seamonkey-2.53.16.es-ES.linux-i686.tar.bz2
 
+echo "3. Engadindo Seamonkey ao escritorio LXDE do usuario..."
 echo "
 [Desktop Entry]
 Name=Navegador web
@@ -25,17 +29,21 @@ Type=Application
 Categories=Network;WebBrowser;
 " > /usr/share/applications/seamonkey.desktop
 
+echo "4. Establecendo usuarios e permisos correctos para o navegador..."
 chown -R usuario:usuario /opt/seamonkey
 chmod -R 755 /opt/seamonkey
-echo "Añadido SeaMonkey como navegador web."
+
 cp /usr/share/applications/seamonkey.desktop /etc/xdg/autostart
 
+echo "5. Activando o arranque da wifi por defecto para o usuario na sesion..."
 # Activamos a wifi no arranque de la sesion
 grep -q "nmcli" /home/usuario/.config/lxsession/LXDE/autostart
 if [ $? -eq 1 ] # Si no encuentra el comando de nmcli lo anhadimos
 then
  echo "nmcli radio wifi on" >> /home/usuario/.config/lxsession/LXDE/autostart
 fi
+
+echo "6. Finalizadas tarefas: instalación de Seamonkey, arranque na aula virtual e activación da Wifi co arranque."
 
 : '
 # Activamos la wifi en el arranque
