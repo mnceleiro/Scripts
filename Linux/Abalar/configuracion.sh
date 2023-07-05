ad96e1608dbf3af1b54ac47e44924f140f18c3b8
Marcos Núñez Celeiro <Markiusarria@gmail.com>
2023-07-05 10:30:32 +0200

    Paxina de inicio Seamonkey a aulavirtual. Actualizado arranque wifi

--- Linux/Abalar/configuracion.sh
+++ Linux/Abalar/configuracion.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-## Este script:
+## Este script está preparado para a versión lixeira (LXDE) do sistema operativo Abalar 11. Fai o seguinte:
 # - Instala SeaMonkey e engadeo ao menu de inicio do sistema.
-# - Arranca automaticamente SeaMonkey no boot
+# - Arranca automaticamente SeaMonkey no boot e inicia coa URL da aula virtual
+# - Arranca a wifi co inicio da sesion de LXDE
 
 # TODO: preguntar o nome de usuario
 
@@ -17,7 +18,7 @@ Name=Navegador web
 GenericName=Navegador web
 X-GNOME-FullName=SeaMonkey
 Comment=Navegador web
-Exec=/opt/seamonkey/seamonkey
+Exec=/opt/seamonkey/seamonkey -url http://www.edu.xunta.gal/centros/iescosmelopez/aulavirtual/login/
 Icon=/opt/seamonkey/chrome/icons/default/default64.png
 Terminal=false
 Type=Application
@@ -29,6 +30,14 @@ chmod -R 755 /opt/seamonkey
 echo "Añadido SeaMonkey como navegador web."
 cp /usr/share/applications/seamonkey.desktop /etc/xdg/autostart
 
+# Activamos a wifi no arranque de la sesion
+grep -q "nmcli" /home/usuario/.config/lxsession/LXDE/autostart
+if [ $? -eq 1 ] # Si no encuentra el comando de nmcli lo anhadimos
+then
+ echo "nmcli radio wifi on" >> /home/usuario/.config/lxsession/LXDE/autostart
+fi
+
+: '
 # Activamos la wifi en el arranque
 echo "
 #!/bin/bash
@@ -52,3 +61,4 @@ chmod 755 /etc/systemd/system/startup-script.service
 systemctl enable startup-script.service
 systemctl daemon-reload
 systemctl start startup-script.service
+'
